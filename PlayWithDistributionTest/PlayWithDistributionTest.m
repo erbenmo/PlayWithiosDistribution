5058c0a6367d35282077d59de946822345cd943f
Erben Mo <moerben@amazon.com>
2012-12-20 11:18:12 -0800

    add test case

---
+++ PlayWithDistributionTest/PlayWithDistributionTest.m
@@ -0,0 +1,32 @@
+//
+//  PlayWithDistributionTest.m
+//  PlayWithDistributionTest
+//
+//  Created by Mo, Erben on 12/20/12.
+//  Copyright (c) 2012 Cloud Drive. All rights reserved.
+//
+
+#import "PlayWithDistributionTest.h"
+
+@implementation PlayWithDistributionTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    // success
+}
+
+@end
---
+++ PlayWithDistributionTest/RandomTest.m
@@ -0,0 +1,24 @@
+//
+//  RandomTest.m
+//  PlayWithDistribution
+//
+//  Created by Mo, Erben on 12/20/12.
+//  Copyright (c) 2012 Cloud Drive. All rights reserved.
+//
+
+#import "RandomTest.h"
+
+@implementation RandomTest
+
+- (void)setUp {
+    // do nothing..
+}
+
+- (void)tearDown {
+    // do nothing...
+}
+
+- (void)testHelloWorld {
+    STFail(@"what??");
+}
+@end
